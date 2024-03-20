9627b51201577fda73af1e3d1ce7f5a9e12dd943
Maksim <sasha.supron3@icloud.com>
2024-03-20 21:29:12 +0100

    test
    proveriayem

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const client = Client({
   personalAccessToken: 'figd_mXj_LUE9P4L2oh1ptGmxkB8-iukihLTchcCP-QUI'
 });
 
-
+// хуй
 client.file('G84li0MrDPWRZGW5lVLIr1').then(file => {
   console.log(file);
 }).catch(error => {
