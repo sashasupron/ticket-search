e946e20559180eb548b2f191b9cf42bb0df3ca2b
Maksim <sasha.supron3@icloud.com>
2024-03-20 23:27:38 +0100

    main_page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,37 @@
 import { Client } from 'figma-js'; 
+import image from './assets/background1.png';
+import './App.css'
 
 
 const client = Client({
   personalAccessToken: 'figd_mXj_LUE9P4L2oh1ptGmxkB8-iukihLTchcCP-QUI'
 });
 
-// хуй
+
 client.file('G84li0MrDPWRZGW5lVLIr1').then(file => {
   console.log(file);
 }).catch(error => {
   console.error(error);
 });
 
+
+
+
+
+
 function App() {
   return (
-    <>
+    <div className="fullscreen">
+      <img src = {image} className = "full-img" />
+        <div className = "mainSearch">
+          <input type = "text" placeholder = "Where from?" />
+          <input type = "text" placeholder = "Where to?" />
+        </div>
+      </div>
 
-    </>
+
+
+
   )
 }
 
