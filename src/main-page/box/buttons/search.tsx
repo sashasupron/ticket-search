076273d3057c1dc8a82e3b9dc02c431ef9ce06b2
Lim0H <bikofusyba55@gmail.com>
2024-04-05 21:25:29 +0200

    Add endpoint TEST

--- src/main-page/box/buttons/search.tsx
+++ src/main-page/box/buttons/search.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import TestClient from '../../../response/test.js';
+
 
 export default function SearchButton() {
   return (
     <Button 
+      onClick={(e: any) => {
+        TestClient.sendGet().then(res => {
+            alert(res.data);
+        })
+      }}
       variant = "outlined" 
       sx = {{ 
         background: 'rgba(250, 149, 44, 0.5)',
