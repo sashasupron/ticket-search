afeb2fdde431b13ffb1e39baa3bb01c9d67086d2
Sasha <sasha.supron3@icloud.com>
2024-10-10 01:44:45 +0200

    poryadok_2

--- src/components/mainPage/mainPage.tsx
+++ src/components/mainPage/mainPage.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
-import image from ".../assets/background2.png"
+import image from "../assets1/background-photoaidcom-darken.jpg";
 import BoxMainPage from "../box/boxMainPage";
-import "./mainPage.moddule.css";
+import "./mainPage.module.css";
 import Description from "../text/bannerText";
 
 function MainPage() {
   return (
     <div className="fullscreen">
-      <Image src={image} className="full-img" alt={"Main page backgroud"} />
+      <Image 
+        src={image} 
+        className="full-img" 
+        alt={"Main page background"} 
+        layout="fill" 
+        objectFit="cover" 
+      />
       <BoxMainPage />
       <Description label="Explore your adventures! Book flights now!" />
     </div>
