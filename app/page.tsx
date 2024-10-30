6843ca8112bbaae14fe5bdda2a7a2ce29d755af1
Illia Bahlai <bahlai.illia@gmail.com>
2024-10-30 09:51:02 +0100

    Fix Router structure and remove not use codes

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import SearchPage from "@/pages/searchPage";
+import TicketPage from "@/pages/ticketPage";
+
+export default function Page() {
+  return (
+    <>
+      <SearchPage />
+      <TicketPage />
+    </>
+  );
+}
--- src/pages/searchPage/ui/searchPage.tsx
+++ src/pages/searchPage/ui/searchPage.tsx
@@ -1,22 +1,21 @@
-import Image from "next/image";
 import image from "@/shared/assets/images/background2-1-photoaidcom-darken.png";
-import { BoxMainPage } from "@/widgets/box/searchPage/ui/boxMainPage";
-import "./searchPage.module.css";
 import Description from "@/shared/ui/banners/bannerText";
+import { BoxMainPage } from "@/widgets/box/searchPage/ui/boxMainPage";
+import Image from "next/image";
+import "./searchPage.module.css";
 
 const SearchPage = () => {
   return (
     <div className="fullscreen">
-      <Image 
-        src={image} 
-        className="full-img" 
-        alt={"Main page background"} 
-        layout="fill" 
-        objectFit="cover" 
+      <Image
+        src={image}
+        className="full-img"
+        alt={"Main page background"}
+        layout="fill"
+        objectFit="cover"
       />
       <BoxMainPage />
       <Description label="Explore your adventures! Book flights now!" />
-  
     </div>
   );
 };
